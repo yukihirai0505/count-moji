4029215eb86a38fe4b1f68600e4f5433d20ee24d
Yuki Hirai <yukihirai0505@gmail.com>
2019-09-02 15:56:48 +0900

    Add sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 import Page from '../components/Page'
@@ -6,24 +6,46 @@ import Container from '../components/Container'
 import IndexLayout from '../layouts'
 import kuromoji from 'kuromoji'
 
+const filterAndSortByValue = (map: Map<string, number>): Map<string, number> => {
+  return new Map([...map.entries()].sort((a, b) => b[1] - a[1]).filter(a => a[1] > 1))
+}
+
 const IndexPage = () => {
-  const text = `おはよう世界`
-  kuromoji.builder({ dicPath: '/dict' }).build((err, tokenizer) => {
-    if (err) {
-      console.log(err)
-    } else {
-      const tokens = tokenizer.tokenize(text)
-      console.log(tokens)
-    }
-  })
+  const [inputVal, setInputVal] = useState('input')
+  const [words, setWords] = useState<Map<string, number>>(() => new Map())
+  const changeInput = (value: string) => {
+    setInputVal(value)
+    kuromoji.builder({ dicPath: '/dict' }).build((err, tokenizer) => {
+      if (err) {
+        console.log(err)
+      } else {
+        const tokens = tokenizer.tokenize(value)
+        const nouns = new Map<string, number>()
+        tokens.forEach(token => {
+          if (token.pos === '名詞') {
+            const word = token.surface_form
+            const count = nouns.get(word)
+            nouns.set(word, count ? count + 1 : 1)
+          }
+        })
+        setWords(filterAndSortByValue(nouns))
+      }
+    })
+  }
+  console.log(words)
   return (
     <IndexLayout>
       <Page>
         <Container>
-          <h1>Hi people</h1>
-          <p>Welcome to your new Gatsby site.</p>
-          <p>Now go build something great.</p>
-          <Link to="/page-2/">Go to page 2</Link>
+          <textarea value={inputVal} onChange={e => changeInput(e.target.value)} rows={10} />
+          <ul>
+            {words &&
+              [...words.entries()].map((word, idx) => (
+                <li key={idx}>
+                  {word[0]} {word[1]}
+                </li>
+              ))}
+          </ul>
         </Container>
       </Page>
     </IndexLayout>
